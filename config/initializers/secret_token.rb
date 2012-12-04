308232ba9de7b153fe7c1829ac1e9bd832c2190a
Shawn Anderson <shawn42@gmail.com>
2012-12-04 00:08:41 -0500

    initial commit of a bundled rails4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bullwinkle::Application.config.secret_key_base = 'ec705826fe5b2609068e9def9e04ddaf5591c2494260d32e58e9326a60a501be9f8d2830f166dbf3a31dad78d963f09c447b6fcd387907bdc6bf9f04a4a4a1a0'
